e4e7d187a9facab26d3e6150ac4d2477bcd2be98
Bangjihoon <jihoon289@naver.com>
2022-03-09 22:47:21 +0900

    프폴 앱 만들기 - 글꼴까지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50, // 반지름
+                backgroundImage: AssetImage('assets/images/bang.jpg'),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+/*Container(
+                  width: 100.0,
+                  height: double.infinity,
+                  color: Colors.red,
+                  child: Text('Container 1')),
+              SizedBox(
+                width: 50,
+              ),
+              Container(
+                  width: 100.0,
+                  height: 100,
+                  color: Colors.yellow,
+                  child: Text('Container 2')),
+              SizedBox(
+                width: 50,
+              ),
+              Container(
+                  width: 100.0,
+                  height: double.infinity,
+                  color: Colors.blue,
+                  child: Text('Container 3')),
+            */
